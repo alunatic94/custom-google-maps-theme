53acdd2e4e4961198ab1cadbd84e03487525d929
alunatic94 <Alberto.luna.642@my.csun.edu>
2022-08-01 14:11:46 -0700

    updated the carousel image slider

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 let collectMarkers=[];
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const images = document.querySelector('.carousel').children;
+const totalImages = images.length;
+let index = 0;
 
+prev.addEventListener('click', () => {
+  nextImage('next');
+})
+
+next.addEventListener('click', () => {
+  nextImage('prev');
+})
+
+function nextImage(direction) {
+  if(direction == 'next') {
+    index++;
+    if(index == totalImages) {
+      index = 0;
+    }
+  } else {
+    if(index == 0) {
+      index = totalImages - 1;
+    } else {
+      index--;
+    }
+  }
+
+  for(let i = 0; i < images.length; i++) {
+    images[i].classList.remove('main');
+  }
+  images[index].classList.add('main');
+}
 function initMap(){
    map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 33.809962404454765, lng: -117.91895316314884},
